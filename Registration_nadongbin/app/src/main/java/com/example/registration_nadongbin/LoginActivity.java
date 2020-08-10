0e60f0acb2e158f84c6e68343dfa3a801354f956
KimHyungSik <colrog968@gmail.com>
2020-08-10 22:59:49 +0900

    Sign Up and phpMyadmin setting

--- Registration_nadongbin/app/src/main/java/com/example/registration_nadongbin/LoginActivity.java
+++ Registration_nadongbin/app/src/main/java/com/example/registration_nadongbin/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.registration_nadongbin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    TextView registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        setFindId();
+        setClickListener();
+
     }
+
+    void setFindId(){
+        registerButton = findViewById(R.id.registerButton);
+    }
+    void setClickListener(){
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+    }
+
 }
